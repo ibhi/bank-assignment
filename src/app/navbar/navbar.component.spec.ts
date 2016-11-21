1f95acec97df3cb8cd25f472d55a3d9af71c2f0e
ibhi <ibrahimin2004@gmail.com>
2016-11-21 09:51:43 +0530

    Done: Unit testing for navbar component

--- src/app/navbar/navbar.component.spec.ts
+++ src/app/navbar/navbar.component.spec.ts
@@ -1,11 +1,41 @@
 /* tslint:disable:no-unused-variable */
 
-import { TestBed, async } from '@angular/core/testing';
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+import { IData } from './../model/data.model';
 import { NavbarComponent } from './navbar.component';
 
 describe('Component: Navbar', () => {
-  it('should create an instance', () => {
-    let component = new NavbarComponent();
-    expect(component).toBeTruthy();
+  let comp: NavbarComponent;
+  let fixture: ComponentFixture<NavbarComponent>;
+  let de: DebugElement;
+  let ne: HTMLElement;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [NavbarComponent]
+    })
+    .compileComponents(); // compiles template and css
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NavbarComponent);
+    comp = fixture.componentInstance;
+    de = fixture.debugElement;
+
+    comp.brandName = 'Rabo Bank';
+
+    fixture.detectChanges();
+    ne = fixture.debugElement.nativeElement;
   });
+
+  it('should create an instance', () => {
+    expect(comp).toBeTruthy();
+  });
+
+  it('should have proper brand name', () => {
+    expect(ne.querySelector('.navbar-brand').textContent).toBe('Rabo Bank');
+  });
+
 });
